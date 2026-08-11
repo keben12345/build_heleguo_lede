793a30383e354b5aaa9359a13156cbeba03587e0
keben12345 <79047578+keben12345@users.noreply.github.com>
2026-08-12 07:35:41 +0800

    Update diy-part2_jcg_q20.sh

--- scripts/diy-part2_jcg_q20.sh
+++ scripts/diy-part2_jcg_q20.sh
@@ -62,7 +62,7 @@ git clone --depth=1 https://github.com/Lienol/openwrt-package
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
 # add ssr+ plus
-git clone --depth=1 https://github.com/fw876/helloworld.git
+git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-vssr <M>
 #git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
